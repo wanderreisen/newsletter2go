c41ca1ab6764ff34ba3191e4faba884cd277a9b9
Konstantin Knopp <k.knopp@das-unikat.com>
2020-04-30 09:44:14 +0200

    update enums

--- src/Newsletter2Go.php
+++ src/Newsletter2Go.php
@@ -7,8 +7,14 @@ use NL2GO\Newsletter2Go_REST_Api;
 class Newsletter2Go
 {
 
+    /**
+     * @var Newsletter2Go_REST_Api
+     */
     private $api;
 
+    /**
+     * Newsletter2Go constructor.
+     */
     public function __construct()
     {
         $api = new Newsletter2Go_REST_Api(config('newsletter2go.api_key'), config('newsletter2go.username'), config('newsletter2go.password'));
@@ -16,6 +22,9 @@ class Newsletter2Go
         $this->api = $api;
     }
 
+    /**
+     * @return Newsletter2Go_REST_Api
+     */
     public function connect()
     {
         return $this->api;
